a7f9c2dc695b62caee265dce9774ba99a7a8698d
lae <lae@lae.is>
2024-08-31 02:08:45 +0900

    return reqwest client directly in new_http_client

--- src/common.rs
+++ src/common.rs
@@ -6,9 +6,7 @@ pub fn new_http_client() -> ClientWithMiddleware {
     // Retry on failure for up to 15 seconds
     let retry_policy =
         ExponentialBackoff::builder().build_with_total_retry_duration(Duration::from_secs(15));
-    let client = ClientBuilder::new(reqwest::Client::new())
+    ClientBuilder::new(reqwest::Client::new())
         .with(RetryTransientMiddleware::new_with_policy(retry_policy))
-        .build();
-
-    client
+        .build()
 }
